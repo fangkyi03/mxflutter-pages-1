cc71168b05fadb9e618fb3f795658db364ff757f
fangkyi03 <fangkyi02@live.cn>
2020-04-05 01:37:19 +0800

    Deploy Next.js to gh-pages

--- _next/static/chunks/styles.8439404b6e8e52fa3ca0.js
+++
@@ -1,1 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[9],{Oa2G:function(p,s,n){}}]);
--- _next/static/w8nnkQFV8EGhQoqldwMEH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002Fb535ac075802629b803a5a09c29150cce5a8ecc8.bebcfb6c9f4928a89389.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
